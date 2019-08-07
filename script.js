2e6de4f31c92a23246cbab008ea9022ea97c271e
Karol Niewiadowski <speakpolishpl@gmail.com>
2019-08-07 15:14:31 +0200

    Added basic functionality to keyboard

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 var number = -1;  // randomised to get a pitch
 var note = 0;  // key-code and data-key representation
 var score = 0;
+var isFlatSharp = -1;
 
 var butts = document.querySelectorAll('.butt'); // buttons with letters
 var result = document.getElementById("result"); // where score is displayed
 var progress = document.getElementById("progress"); // timer
+
+var whites = document.querySelectorAll(".white");
+var blacks = document.querySelectorAll(".black, .spacer");
 
 var sharp = document.querySelector('#sharp');
 var flat = document.querySelector('#flat');
@@ -22,7 +26,7 @@ var bass = false;    // bass control
 
 function RandomNote() {
     var top = 1;  // Elevation of note
-    var temp;  // For {number} to not repeat
+    var temp;  // Avoids repeating {number}
 
     if (!cycle) {
         cycle = Math.floor(Math.random() * 5) + 5;
@@ -33,7 +37,7 @@ function RandomNote() {
             isBass = true;
         }
     }
-    
+
     if (!isBass) {
         document.getElementById("key").style.opacity = 1;
         document.getElementById("bass").style.opacity = 0;
@@ -136,6 +140,75 @@ function RandomNote() {
         }
     }
 
+    // correct key in keyboard
+
+    if (isFlatSharp != 0 && isFlatSharp != 1) {
+        if (isBass) {
+            whites[15 - number].classList.add("piano-answer");
+        }
+        else {
+            whites[27 - number].classList.add("piano-answer");
+        }
+    }
+    else if (isFlatSharp == 1) { // flat
+        if (isBass) {
+            switch (number) {
+                case 1:
+                case 5:
+                case 8:
+                case 12:
+                    whites[14 - number].classList.add("piano-answer");
+                    break;
+                default:
+                    blacks[14 - number].classList.add("piano-answer");
+                    break;
+            }
+        }
+        else {
+            switch (number) {
+                case 3:
+                case 6:
+                case 10:
+                case 13:
+                    whites[26 - number].classList.add("piano-answer");
+                    break;
+                default:
+                    blacks[26 - number].classList.add("piano-answer");
+                    break;
+            }
+        }
+    }
+    else if (isFlatSharp == 0) { // sharp
+        if (isBass) {
+            switch (number) {
+                case 2:
+                case 6:
+                case 9:
+                case 13:
+                    whites[16 - number].classList.add("piano-answer");
+                    break;
+                default:
+                    blacks[15 - number].classList.add("piano-answer");
+                    break;
+            }
+        }
+        else {
+            switch (number) {
+                case 0:
+                case 4:
+                case 7:
+                case 11:
+                case 14:
+                    whites[28 - number].classList.add("piano-answer");
+                    break;
+                default:
+                    blacks[27 - number].classList.add("piano-answer");
+                    break;
+            }
+        }
+    }
+
+
 
     // shows additional lines if needed
     var short = document.querySelectorAll('.short');  // short lines above and below
@@ -153,17 +226,17 @@ function RandomNote() {
 }
 
 // # and b
-function FlatSharp(){
-    var isFlatSharp = Math.floor(Math.random() * 10);
-    if(isFlatSharp == 0){
+function FlatSharp() {
+    isFlatSharp = Math.floor(Math.random() * 2);
+    if (isFlatSharp == 0) {
         sharp.style.opacity = 1;
         flat.style.opacity = 0;
     }
-    else if(isFlatSharp == 1){
+    else if (isFlatSharp == 1) {
         sharp.style.opacity = 0;
         flat.style.opacity = 1;
     }
-    else{
+    else {
         sharp.style.opacity = 0;
         flat.style.opacity = 0;
     }
@@ -194,13 +267,34 @@ butts.forEach(b => b.addEventListener('click', function (e) {
         correctKey();
         RandomNote();
     }
-    // do poprawy
     else {
         wrongKey();
     }
-    //
+
     var butt = document.querySelector(`.butt[data-key="${this.dataset['key']}"]`);
     butt.classList.add("playing");
+}));
+
+whites.forEach(w => w.addEventListener('click', function (e) {
+
+    if (this.classList.contains("piano-answer")) {
+        correctKey();
+        RandomNote();
+    }
+    else {
+        wrongKey();
+    }
+}));
+
+blacks.forEach(b => b.addEventListener('click', function (e) {
+
+    if (this.classList.contains("piano-answer")) {
+        correctKey();
+        RandomNote();
+    }
+    else {
+        wrongKey();
+    }
 }));
 
 
@@ -240,6 +334,7 @@ function correctKey() {
     var audio = document.getElementsByClassName("correct")[0];
     audio.play();
     audio.classList.remove("correct");
+    document.getElementsByClassName("piano-answer")[0].classList.remove("piano-answer");
 
     if (!ishelp) {
         score++;
@@ -301,32 +396,32 @@ halpbut.addEventListener("click", function (e) {
 var wiolinControl = document.getElementsByClassName("controls")[0];
 var bassControl = document.getElementsByClassName("controls")[1];
 
-wiolinControl.addEventListener("click", function(e){
-    if(wiolin){
+wiolinControl.addEventListener("click", function (e) {
+    if (wiolin) {
         wiolin = false;
         cycle = 1;
 
         wiolinControl.classList.remove("control-pressed");
     }
-    else{
+    else {
         bass = false;
         wiolin = true;
         isBass = false;
         cycle = -1;
-        
+
         wiolinControl.classList.add("control-pressed");
         bassControl.classList.remove("control-pressed");
     }
 })
 
-bassControl.addEventListener("click", function(e){
-    if(bass){
+bassControl.addEventListener("click", function (e) {
+    if (bass) {
         bass = false;
         cycle = 1;
 
         bassControl.classList.remove("control-pressed");
     }
-    else{
+    else {
         bass = true;
         wiolin = false;
         isBass = true;
